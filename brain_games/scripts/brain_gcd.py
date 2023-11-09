a516b70e54b78bbb27f8fa850e321acc8f665ce9
Gadjijka <galimovgadjimurad2020@gmail.com>
2023-11-09 14:24:35 +0300

    i forgot to add space in gcd game

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -14,16 +14,16 @@ def main():
    while i<3:
         number1=random.randint(0,100)
         number2=random.randint(0,100)
-        print("Question:" + str(number1)+" "+ str(number2))
+        print("Question: " + str(number1)+" "+ str(number2))
         answer=prompt.string('Your Answer:')
         if answer != str(math.gcd(number1,number2)):
-            print("'"+answer+"'"+"is wrong answer ;(. Correct answer was '"+str(math.gcd(number1,number2))+"'.")
+            print("'"+answer+"'"+" is wrong answer ;(. Correct answer was '"+str(math.gcd(number1,number2))+"'.")
             print("Let's try again,"+ " " + name+"!")
             sys.exit()
         print("Correct!")
         i+=1
 
-   print("Congratulations,"+name+'!')
+   print("Congratulations, "+name+'!')
 
 
 if __name__ == 'main':
