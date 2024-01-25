87ea83f5aa1d6b68cc0b71ba9046376328b4cd12
Gadjijka <149161418+Gadjijka@users.noreply.github.com>
2024-01-25 22:28:56 +0300

    Update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,40 +1,10 @@
-import random
-import sys
-import prompt
-from brain_games import cli
+from brain_games.game_logic import play_game
+from brain_games.games import calc
 
 
 def main():
-    cli.welcome_Tirion()
-    print("Welcome to the Brain Games!")
-    name = cli.welcome_user()
-    print("What is the result of the expression?")
-    i = 0
-    sum = 0
-    while i < 3:
-        sign = random.randint(0, 2)
-        number1 = random.randint(100, 200)
-        number2 = random.randint(0, 100)
-        if sign == 0:
-            print("Question: " + str(number1) + " + " + str(number2))
-            sum = number1 + number2
-        if sign == 1:
-            sum = number1 - number2
-            print("Question: " + str(number1) + " - " + str(number2))
-        if sign == 2:
-            sum = number1 * number2
-            print("Question: " + str(number1) + " * " + str(number2))
-        answer = prompt.string('Your Answer:')
-        if answer != str(sum):
-            print("'" + str(answer) + "'" + "is wrong answer"
-                  ";(.Correct answer was '" + str(sum) + "'.")
-            print("Let's try again," + " " + name + "!")
-            sys.exit()
-        print("Correct!")
-        i += 1
-
-    print("Congratulations, " + name + '!')
+    play_game(calc)
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
