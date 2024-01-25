4801d51cdec40697cf4b78d89cccfbd31d220201
Gadjijka <149161418+Gadjijka@users.noreply.github.com>
2024-01-25 22:32:52 +0300

    Update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,43 +1,10 @@
-import random
-import sys
-import prompt
-from brain_games import cli
+from brain_games.game_logic import play_game
+from brain_games.games import brain_progression
 
 
 def main():
-    print("What number is missing in the progression?")
-    cli.welcome_Tirion()
-    print("Welcome to the Brain Games!")
-    name = cli.welcome_user()
-    i = 0
-    while i < 3:
-        number1 = random.randint(0, 100)
-        progression = []
-        progression.append(number1)
-        number2 = random.randint(0, 100)
-        k = 1
-        while k < 10:
-            number1 += number2
-            progression.append(number1)
-            k += 1
-        index = random.randint(0, 9)
-        temporary_number = progression[index]
-        progression[index] = ".."
-        s = ""
-        for char in progression:
-            s += " " + str(char)
-        print("Question:" + s)
-        answer = prompt.string('Your Answer:')
-        if answer != str(temporary_number):
-            print("'" + answer + "'" + "is wrong answer"
-                  " ;(. Correct answer was '" + str(temporary_number) + "'.")
-            print("Let's try again," + " " + name + "!")
-            sys.exit()
-        print("Correct!")
-        i += 1
-
-    print("Congratulations, " + name + '!')
+    play_game(brain_progression)
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
