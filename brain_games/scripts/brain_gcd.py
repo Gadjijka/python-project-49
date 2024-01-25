6b8150b7840a51171bf8a3371a8019bbf697e90e
Gadjijka <149161418+Gadjijka@users.noreply.github.com>
2024-01-25 22:31:32 +0300

    Update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,32 +1,11 @@
-import random
-import sys
-import prompt
-from brain_games import cli
-import math
+from brain_games.game_logic import play_game
+from brain_games.games import brain_gcd
 
 
 def main():
-    cli.welcome_Tirion()
-    print("Welcome to the Brain Games!")
-    name = cli.welcome_user()
-    print("Find the greatest common divisor of given numbers.")
-    i = 0
-    while i < 3:
-        number1 = random.randint(0, 100)
-        number2 = random.randint(0, 100)
-        print("Question: " + str(number1) + " " + str(number2))
-        answer = prompt.string('Your Answer:')
-        if answer != str(math.gcd(number1, number2)):
-            print("'" + answer + "'" + " is wrong answer"
-                  ";(. Correct answer"
-                  "was '" + str(math.gcd(number1, number2)) + "'.")
-            print("Let's try again," + " " + name + "!")
-            sys.exit()
-        print("Correct!")
-        i += 1
-
-    print("Congratulations, " + name + '!')
+    play_game(brain_gcd)
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
+
