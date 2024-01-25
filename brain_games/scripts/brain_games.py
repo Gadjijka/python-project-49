352d9c2ebea25df0c0c25305bd11201ba28e6935
Gadjijka <149161418+Gadjijka@users.noreply.github.com>
2024-01-25 22:30:58 +0300

    Update brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-from brain_games import cli
+from brain_games.cli import welcome_user
 
 
 def main():
-    cli.welcome_Tirion()
-    print("Welcome to the Brain Games!")
-    cli.welcome_user()
+    welcome_user()
 
 
 if __name__ == '__main__':
