b83be064af879e8ffd7a1e1e8f41e7d00d0ee38c
Gadjijka <149161418+Gadjijka@users.noreply.github.com>
2024-01-25 22:30:36 +0300

    Update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,45 +1,10 @@
-import random
-import sys
-import prompt
-from brain_games import cli
+from brain_games.game_logic import play_game
+from brain_games.games import brain_even
 
 
 def main():
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    cli.welcome_Tirion()
-    print("Welcome to the Brain Games!")
-    name = cli.welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    i = 0
-    while i < 3:
-        number = random.randint(0, 100)
-        print('Question:', number)
-        answer = prompt.string('Your Answer:')
-        if answer != 'no' and answer != 'yes':
-            print("Incorrect answer. Let's try again")
-            sys.exit()
-        if number % 2 == 0:
-            if answer == 'yes':
-                print('Correct!')
-                i += 1
-                continue
-            else:
-                print("'no' is wrong answer ;(."
-                      "Correct answer was 'yes'."
-                      "Let's try again," + " " + name + "!")
-                sys.exit()
-        else:
-            if answer == 'no':
-                print('Correct!')
-                i += 1
-                continue
-            else:
-                print("'yes' is wrong answer ;(."
-                      "Correct answer was 'no'."
-                      "Let's try again," + " " + name + "!")
-                sys.exit()
-    print("Congratulations, " + name + '!')
+    play_game(brain_even)
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
