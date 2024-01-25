3aa834f983aca015c51a38653560d3acf42bb880
Gadjijka <149161418+Gadjijka@users.noreply.github.com>
2024-01-25 22:29:55 +0300

    Update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,9 @@
 from brain_games.game_logic import play_game
-from brain_games.games import calc
+from brain_games.games import brain_calc
 
 
 def main():
-    play_game(calc)
+    play_game(brain_calc)
 
 
 if __name__ == '__main__':
