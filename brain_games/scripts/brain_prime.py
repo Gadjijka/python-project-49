2431f2cc97c0c46d517235433f1c177799ead8d5
Gadjijka <149161418+Gadjijka@users.noreply.github.com>
2024-01-25 22:32:19 +0300

    Update brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,36 +1,10 @@
-import random
-import sys
-import prompt
-from brain_games import cli
+from brain_games.game_logic import play_game
+from brain_games.games import brain_prime
 
 
 def main():
-    cli.welcome_Tirion()
-    print("Welcome to the Brain Games!")
-    name = cli.welcome_user()
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    i = 0
-    while i < 3:
-        number = random.randint(0, 100)
-        print('Question: ' + str(number))
-        answer = prompt.string('Your Answer:')
-        k = 0
-        for r in range(2, number // 2 + 1):
-            if number % r == 0:
-                k = k + 1
-        if k == 0 and answer == 'yes':
-            print("Correct!")
-            i = i + 1
-            continue
-        if k > 0 and answer == 'no':
-            print("Correct!")
-            i = i + 1
-            continue
-        print("'yes' is wrong answer ;(. Correct answer was 'no'.")
-        print("Let's try again, " + name + '!')
-        sys.exit()
-    print("Congratulations, " + name + '!')
+    play_game(brain_prime)
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
