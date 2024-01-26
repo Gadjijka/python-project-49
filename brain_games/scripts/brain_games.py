b17243fece3cc184896533cc0386bdb5b552ccc6
Gadjijka <149161418+Gadjijka@users.noreply.github.com>
2024-01-26 14:18:46 +0300

    Update brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 from brain_games.cli import welcome_user
-from brain_games.game_logic import play_game
-from brain_games.games import brain_even, brain_calc, brain_gcd, brain_prime, brain_progression
 
 
 def main():
