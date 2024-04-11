d369a3efaa0686c79dd398566c3c7aa974827eb2
Gadjijka <149161418+Gadjijka@users.noreply.github.com>
2024-04-12 01:41:53 +0300

    Update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,9 +1,9 @@
 from brain_games.game_logic import play_game
-from brain_games.games import brain_gcd
+from brain_games.games import gcd
 
 
 def main():
-    play_game(brain_gcd)
+    play_game(gcd)
 
 
 if __name__ == '__main__':
