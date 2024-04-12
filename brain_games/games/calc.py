2c470b07681f7d7b3bf602ac837cc8b88b0e1591
Gadjijka <149161418+Gadjijka@users.noreply.github.com>
2024-04-12 19:11:37 +0300

    Update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,14 +3,7 @@ import random
 RULES = 'What is the result of the expression?'
 
 
-def make_expression():
-    """
-    Generating expression
-    """
-    list_of_expressions = ['+', '-', '*']
-    first_number = random.randint(1, 100)
-    second_number = random.randint(1, 100)
-    operator = random.choice(list_of_expressions)
+def calculate_an_expression(first_argument, operator, second_number):
     right_answer = 0
     match operator:
         case '+':
@@ -19,7 +12,18 @@ def make_expression():
             right_answer = first_number - second_number
         case '*':
             right_answer = first_number * second_number
-    return f'{first_number} {operator} {second_number}', right_answer
+    return right_answer
+
+
+def make_expression():
+    """
+    Generating expression
+    """
+    list_of_expressions = ['+', '-', '*']
+    first_number = random.randint(1, 100)
+    second_number = random.randint(1, 100)
+    operator = random.choice(list_of_expressions)
+    return f'{first_number} {operator} {second_number}', calculate_an_expression(first_number, operator, second_number)
 
 
 def generate_round():
